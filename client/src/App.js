ee0fc42b65b47bba2ee21e29a19e26a30b48140f
Evan Rosse <evanrosse@gmail.com>
2022-11-08 08:56:31 -0500

    create router component which accepts a user prop to refactor into usecontext user provider

--- client/src/App.js
+++ client/src/App.js
@@ -1,33 +1,20 @@
 import React, { useContext, useState, useEffect } from 'react';
-import { Route, Routes, Navigate } from 'react-router-dom';
-import SignUp from './components/SignUp/signup';
-import LogIn from './components/LogIn/login';
-import Roster from './components/Roster/Roster';
-import Users from './components/Users/Users';
-import StandardLayout from './components/LayoutComponents/StandardLayout/StandardLayout';
-import ContentContainer from './components/LayoutComponents/ContentContainer/ContentContainer';
-import Navagation from './components/LayoutComponents/Navagation/Navagation';
+import Router from './components/Router/Router';
 import axios from 'axios';
 import './App.css';
 
-
-
 function App() {
-  console.log('render')
-  const [currentUser, setCurrentUser] = useState(null);
-  const user = localStorage.getItem("token");
+  const [user, setUser] = useState(null);
+  const [isLoading, setIsLoading] = useState(true)
+  // const user = localStorage.getItem("token");
   // const UserContext = useContext(null);
-  // useEffect(async () => {
-  //   const url = 'http://localhost:5500/api/auth';
-  //   console.log('user', user)
-  //   const userData  = await axios.get(url, user);
-  //   console.log(userData);
-  // }, [user]);
+
   const getUser = async (token) => {
     const url = 'http://localhost:5500/api/auth';
     const userData  = await axios.get(url, { headers: {"Authorization" : `Bearer ${token}`} });
     console.log('USER DATA ON LOAD', userData);
-    // setCurrentUser(userData.data.user);
+    setUser(userData.data.user);
+    setIsLoading(false);
     return;
   }
 
@@ -35,26 +22,21 @@ function App() {
     if (localStorage.getItem("token")) {
       const token = localStorage.getItem("token")
       getUser(token);
+    } else {
+      setIsLoading(false);
     }
-  }, [currentUser]);
+  }, []);
+
+  console.log("user App.js", user);
 
   return (
-        <div className="App">  
-          <Routes>   
-            {user && <Route path="/" exact element={<Navigate replace to="/welcome" />} />}
-            {user && <Route path="/roster" exact element={<Roster />} />}
-            <Route path="/users" exact element={<Users />} />
-            <Route path="/signup" exact element={<SignUp />} />
-            <Route path="/login" exact element={<LogIn />} />
-            <Route path="/" exact element={<Navigate replace to="/login" />} />
-            <Route path="/roster" exact element={<Navigate replace to="/login" />} />
-            <Route path="/welcome" exact element={
-              <StandardLayout>
-                <Navagation />
-                <ContentContainer /> 
-              </StandardLayout>
-            } />
-            </Routes>
+        <div className="App">
+          { isLoading && 
+            <div>Loading...</div>
+          }
+          { !isLoading &&
+            <Router user={user}/>
+          }
         </div>
   );
 }
---
+++ client/src/components/Router/Router.js
@@ -0,0 +1,36 @@
+import React, { useContext, useState, useEffect } from 'react';
+import { Route, Routes, Navigate } from 'react-router-dom';
+import SignUp from '../SignUp/signup';
+import LogIn from '../LogIn/login';
+import Roster from '../Roster/Roster';
+import Users from '../Users/Users';
+import StandardLayout from '../LayoutComponents/StandardLayout/StandardLayout';
+import ContentContainer from '../LayoutComponents/ContentContainer/ContentContainer';
+import Navagation from '../LayoutComponents/Navagation/Navagation';
+
+
+
+function Router(props) {
+  const {user} = props; 
+  console.log('render', user)
+
+  return (
+    <Routes>  
+      {user && <Route path="/" exact element={<Navigate replace to="/welcome" />} />}
+      {user && <Route path="/roster" exact element={<Roster />} />}
+      <Route path="/users" exact element={<Users />} />
+      <Route path="/signup" exact element={<SignUp />} />
+      <Route path="/login" exact element={<LogIn />} />
+      <Route path="/" exact element={<Navigate replace to="/login" />} />
+      <Route path="/roster" exact element={<Navigate replace to="/login" />} />
+      <Route path="/welcome" exact element={
+        <StandardLayout>
+          <Navagation />
+          <ContentContainer /> 
+        </StandardLayout>
+      } />
+      </Routes>
+  );
+}
+
+export default Router;
